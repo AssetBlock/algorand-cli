c08486ed15bd037c7eb51e52e82700adce2ba6c7
Jay Ciruolo <jciruolo@gmail.com>
2019-11-20 08:55:54 -0500

    Add UI Boilerplate

---
+++ packages/cli-ui/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Algorand logo" src="../assets/logo.svg" />
+    <HelloWorld msg="Welcome to Algorand" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
